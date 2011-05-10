453ce15f070548cb336ce212c5727651052fbdf9
Pablo Recio Quijano <precio@yaco.es>
2011-05-10 15:48:02 +0200

    Added base registration_form template

--- peer/settings.py
+++ peer/settings.py
@@ -28,6 +28,10 @@
 
 # Django settings for peer project.
 
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -134,6 +138,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASEDIR, 'templates'),
 )
 
 INSTALLED_APPS = (
