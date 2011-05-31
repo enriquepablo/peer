9a54d708ffd0b43b8f08cbdb210c27ac1ad4c0d0
Pablo Recio Quijano <precio@yaco.es>
2011-05-31 13:00:40 +0200

    Added media directory to the urls.

--- peer/urls.py
+++ peer/urls.py
@@ -28,6 +28,7 @@
 
 
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 
 from captcha.forms import RegistrationFormCaptcha
@@ -38,6 +39,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 
     url(r'^accounts/register/$',
         'registration.views.register',
