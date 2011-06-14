0494fbcff7e5dc4a6b656cb140c197df2bb98f60
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2011-06-14 11:18:02 +0200

    Small css refactoring to maximize rules reuse. Now the edit metadata view can display the entity menu on the right

--- peer/staticfiles/js/jquery.accordion.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-    $(document).ready(function(){
-        $("#accordion").accordion();
-        var active = parseInt($("#active_fold").val(), 10);
-        $('#accordion').accordion("activate", active);
-    });
-})(jQuery);
