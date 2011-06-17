379dd72e8598641d10065597cab92218e97892ae
Enrique Perez Arnaud <enriquepablo@gmail.com>
2011-06-17 14:59:59 +0200

    add remainder for full text column

--- peer/entity/migrations/0006_manual__add_full_text_index_on_entity_name.py
+++ peer/entity/migrations/0006_manual__add_full_text_index_on_entity_name.py
@@ -8,6 +8,10 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         if db.engine == 'south.db.postgresql_psycopg2':
             lang = getattr(settings, 'PG_FT_INDEX_LANGUAGE', 'english')
+            # XXX If we create a column of type tsvector on name (...)
+            # we avoid the call to to_tsvertor in the query and gain efficiency.
+            # We then have to add a trigger on inserts, see de pg docs:
+            # http://www.postgresql.org/docs/8.4/static/textsearch.html
             db.db.execute("CREATE INDEX entity_entity_name_idx"
                        " ON entity_entity"
                        " USING gin(to_tsvector('%s', name));" % lang)
