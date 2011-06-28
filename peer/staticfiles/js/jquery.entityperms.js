4ef1696d4a66afc3c1c611cb97e5ea1180e851dc
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2011-06-28 15:45:14 +0200

    Use a namespace object to avoid name collisions in the jQuery global object

--- peer/staticfiles/js/jquery.entityperms.js
+++ peer/staticfiles/js/jquery.entityperms.js
@@ -1,6 +1,8 @@
 (function( $ ){
 
-    $.fn.load_delegates = function(entity_id) {
+    $.fn.delegates = {};  // namespace
+
+    $.fn.delegates.load_delegates = function(entity_id) {
         $.get('/entity/'+entity_id+'/list_delegates/',
             function (html) {
                 $('#delegates-list').html(html);
@@ -9,13 +11,13 @@
         );
     };
 
-    $.fn.select_first_user = function() {
+    $.fn.delegates.select_first_user = function() {
         var q = $('input#q').val();
         $.getJSON('/accounts/search_users_auto/?term='+q,
             function (resp) {
                 for (i in resp) {
                     if (resp[i]["value"] == q) {
-                        $.fn.add_selected_delegate();
+                        $.fn.delegates.add_selected_delegate();
                         $('button#add-delegate').attr('disabled', true);
                         return;
                     }
@@ -28,7 +30,7 @@
         return false;
     };
 
-    $.fn.remove_delegate = function(entity_id, user_id) {
+    $.fn.delegates.remove_delegate = function(entity_id, user_id) {
         $.get('/entity/'+entity_id+'/remove_delegate/'+user_id,
             function (html) {
                 $('div#delegates-list').html(html);
@@ -38,15 +40,15 @@
         return false;
     };
 
-    $.fn.add_selected_delegate = function() {
+    $.fn.delegates.add_selected_delegate = function() {
         var entity_id = $('input#entity_id').val()
         var username = $('input#q').val()
         $.get('/entity/'+entity_id+'/add_delegate/'+username,
             function (html) {
                 if (html == 'delegate') {
-                    $.fn.team_perm_message(username+' can already edit this entity');
+                    $.fn.delegates.team_perm_message(username+' can already edit this entity');
                 } else if (html == 'owner') {
-                    $.fn.team_perm_message(username+' is the owner this entity');
+                    $.fn.delegates.team_perm_message(username+' is the owner this entity');
                 } else {
                     $('div#delegates-list').html(html);
                     $('button#add-delegate').attr('disabled', true);
@@ -57,17 +59,17 @@
         return false;
     };
 
-    $.fn.enable_add_delegate = function () {
+    $.fn.delegates.enable_add_delegate = function () {
         $('button#add-delegate').attr('disabled', false);
     };
 
-    $.fn.disable_add_delegate = function (event) {
+    $.fn.delegates.disable_add_delegate = function (event) {
         if (event.keyCode != 13) {
             $('button#add-delegate').attr('disabled', true);
         }
     };
 
-    $.fn.change_owner = function () {
+    $.fn.delegates.change_owner = function () {
         if (confirm('Only the owner can edit the team permissions of an entity,\n'+
                    'and there can be only one owner for each entity.\n'+
                    'Therefore, you will not be able to undo this action.\n\n'+
@@ -78,7 +80,7 @@
         }
     };
 
-    $.fn.team_perm_message = function (msg) {
+    $.fn.delegates.team_perm_message = function (msg) {
         $('ul#messages').html('<li>'+msg+'</li>')
                         .fadeIn()
                         .delay(3000)
