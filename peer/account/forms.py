aa5f8a426cd86cd34d9a9244fe0d020969b7c123
Danny Navarro <j@dannynavarro.net>
2011-11-17 15:55:56 +0100

    Email field is already mandatory.

--- peer/account/forms.py
+++ peer/account/forms.py
@@ -39,12 +39,6 @@ class RegistrationFormCaptchaTOU(RegistrationFormCaptcha):
 
     tou = TermsOfUseField(readtou('USER_REGISTER_TERMS_OF_USE'))
 
-    def __init__(self, *args, **kwargs):
-        super(RegistrationFormCaptcha, self).__init__(*args, **kwargs)
-        self.fields['email'].help_text = _(
-            u"You need an email address in case you need to reset your "
-            u"password.")
-
 
 class PersonalInformationForm(forms.ModelForm):
 
