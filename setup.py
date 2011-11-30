a2248dc6228b8b7ab88b74da97348512ec81928c
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2011-11-30 22:44:45 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def recursive_include(directory, patterns):
 
 # be careful with the syntax of this line since it is parsed from
 # the docs/conf.py file
-VERSION = '0.6.0'
+VERSION = '0.6.1dev'
 
 setup(
     name='peer',
