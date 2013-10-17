8accbcdc831892268b826234476d3ed2b1014234
Alejandro Blanco <ablanco@yaco.es>
2013-10-17 12:16:18 +0200

    Enforce that KeyDescriptor nodes appear before NameIDFormat ones
    Inside a SSO, the KeyDescriptor must appear in the XML tree before
    the NameIDFormat nodes.

--- peer/staticfiles/js/SAMLmetaJS/samlmeta.xml.js
+++ peer/staticfiles/js/SAMLmetaJS/samlmeta.xml.js
@@ -243,7 +243,8 @@ SAMLmetaJS.xmlupdater = function(xmlstring) {
 				[
 					{'localName': 'SingleLogoutService', 'namespaceURI': SAMLmetaJS.Constants.ns.md},
 					{'localName': 'AssertionConsumerService', 'namespaceURI': SAMLmetaJS.Constants.ns.md},
-					{'localName': 'AttributeConsumingService', 'namespaceURI': SAMLmetaJS.Constants.ns.md}
+					{'localName': 'AttributeConsumingService', 'namespaceURI': SAMLmetaJS.Constants.ns.md},
+					{'localName': 'NameIDFormat', 'namespaceURI': SAMLmetaJS.Constants.ns.md}
 				]
 			));
 
